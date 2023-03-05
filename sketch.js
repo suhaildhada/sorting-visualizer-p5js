40cf16a0661e86ad6679f16ac00cf71c8e543525
Suhail Dhada <suhaildhada123@gmail.com>
2023-03-05 17:38:27 +0530

    made it beautiful and added array size slider

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ import { radixSort } from "./algo/radixsort.js";
 import { generateRandomArray } from "./helpers/generateRandomArray.js";
 import { isArraySorted } from "./helpers/isArraySorted.js";
 let values = [];
-let w = 50;
+let w;
 let frameRate = 120;
 let states = [];
 
@@ -17,7 +17,9 @@ const pivotState = 2;
 const state1 = 0;
 const state2 = 1;
 const sortedState = 100;
+const maxSize = 101;
 
+let container = document.getElementById("container");
 let quicksortBtn = document.getElementById("quick-sort");
 let bubblesortBtn = document.getElementById("bubble-sort");
 let selectionSortBtn = document.getElementById("selection-sort");
@@ -26,14 +28,22 @@ let radixSortBtn = document.getElementById("radix-sort");
 let mergeSortBtn = document.getElementById("merge-sort");
 let insertionSortBtn = document.getElementById("insertion-sort");
 let heapSortBtn = document.getElementById("heap-sort");
+let slider = document.getElementById("array-size");
 
 let sorting = false;
 let sorted = false;
 new p5((p5) => {
     p5.setup = () => {
-        p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        let canvasHeight = p5.windowHeight - container.offsetHeight;
+        p5.createCanvas(p5.windowWidth, canvasHeight);
         p5.frameRate(frameRate);
+        w = slider.value;
         generateRandomArray(p5, w, values);
+        slider.oninput = function () {
+            if (sorting) return;
+            w = maxSize - this.value;
+            generateRandomArray(p5, w, values, sorting);
+        };
         quicksortBtn.addEventListener("click", () => {
             sorting = true;
             quickSort(values, 0, values.length - 1, states, sorting);
